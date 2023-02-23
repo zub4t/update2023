d180a6a61d69eb73926c606955f362b51ffee27b
marco <up201800388@up.pt>
2023-02-23 16:33:47 +0000

    almost done

---
+++ flask_server.py
@@ -0,0 +1,113 @@
+# flask_server.py
+from flask import Flask, request, Response, render_template, jsonify
+import pandas as pd
+import uuid
+import qrcode
+from pathlib import Path
+import io
+import os
+import base64
+import json
+app = Flask(__name__,template_folder='/root/templates')
+
+
+uuid_email_mapping = {}
+uuid_points_mapping ={}
+@app.route('/score-points-to/<uuid>', methods=['GET'])
+def score_points_to(uuid):
+    # Check if the uuid exists in the csv file
+    try:
+        df = pd.read_csv('data.csv')
+    except FileNotFoundError:
+        return 'CSV file not found', 404
+
+    if uuid not in df['uuid'].values:
+        return f'uuid {uuid} not found in the csv file', 404
+    
+    # Create the dicts if they do not exist yet
+    if uuid not in uuid_email_mapping:
+        email = df[df['uuid'] == uuid]['email'].values[0]
+        uuid_email_mapping[uuid] = email
+    
+    if uuid not in uuid_points_mapping:
+        uuid_points_mapping[uuid] = 0
+    
+    # Update the points for the uuid
+    points = 10.0
+    uuid_points_mapping[uuid] += points
+    write_dict_to_csv(uuid_email_mapping,"uuid_email_mapping",True)
+    write_dict_to_csv(uuid_points_mapping,"uuid_points_mapping",True)
+
+    return f'Points for uuid {uuid} updated to {uuid_points_mapping[uuid]}', 200
+
+@app.route("/process", methods=["POST"])
+def process_form():
+    # Retrieve the data from the form submission
+    form_data = request.form
+    for key, value in form_data.items():
+        print(f"{key}: {value}")
+    form_data = add_uuid_field(form_data)
+    qr_img = generate_qr_code(form_data)
+    store_values_in_csv(form_data)
+    qr_code_image_b64 = base64.b64encode(qr_img).decode("utf-8")
+    # Do something with the form data (e.g., store it in a database, send it in an email, etc.)
+    #return Response(qr_img, content_type="image/png")
+    return render_template("response.html", qr_code_image_b64=qr_code_image_b64)
+
+@app.route('/workshops/', methods=["GET"])
+def get_workshops():
+    with open('/usr/share/nginx/html/update2023/schedule.json', 'r') as file:
+        data = json.load(file)
+    return jsonify(data)
+
+def add_uuid_field(data):
+    data = data.copy() # make a copy of the ImmutableMultiDict to allow modifications
+    uid = str(uuid.uuid4())
+    data.update({'uuid': uid}) # add the new field
+    print(data)
+    return data
+    
+def write_dict_to_csv(data,filename,write_index):
+    df = pd.DataFrame.from_dict(data, orient='index')
+    df.to_csv(filename, index=write_index)
+
+def store_values_in_csv(data):
+    filename = 'data.csv'
+    fields = ['fst_name', 'lst_name', 'email', 'city', 'state','uuid']
+    
+    # Check if the file exists, if not create it
+    if(not(os.path.isfile(filename))):
+        df = pd.DataFrame(columns=fields)
+        df.to_csv(filename, index=False)
+
+   
+    # Append the data to the csv file
+    df = pd.read_csv(filename)
+    row = {field: data.get(field) for field in fields}
+    df = df.append(row, ignore_index=True)
+    df.to_csv(filename, index=False)
+
+def generate_qr_code(data):
+    uid =  data["uuid"]
+    email = data["email"]
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    url = f"{data['uuid']}"
+    qr.add_data(url)
+    qr.make(fit=True)
+
+    qr_path = Path("./qrCodes") / email
+    qr_path.mkdir(parents=True, exist_ok=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    output = io.BytesIO()
+    img.save(output)
+    with open(qr_path / f"{email}.png", "wb") as f:
+        img.save(f)
+    return output.getvalue()
+
+if __name__ == "__main__":
+    app.run(port=8000)
---
+++ site/update2023/main.js
@@ -0,0 +1,219 @@
+var scheduleData = null;
+var scheduleEvents1 = [];
+var scheduleEvents2 = [];
+var overlay;
+
+window.addEventListener("load", function(event) {
+    getScheduleContent()
+    getPartnersContent()
+    showSlides(0);
+    v1 = getRandomIntInclusive(0, window.innerHeight / 2)
+    v2 = getRandomIntInclusive(window.innerHeight / 2, window.innerHeight)
+    Array(2).fill(0).map((_, i) => {
+
+        let triangle_div = document.createElement("div");
+        triangle_div.className = "triangle"
+        triangle_div.style.top = `${getRandomIntInclusive(0, window.innerHeight)}px`;
+        //  this.document.body.appendChild(triangle_div)
+
+
+    });
+
+
+});
+
+function getPartnersContent() {
+    fetch('./partner.json')
+        .then(response => response.json())
+        .then(data => {
+            for (p of data.partners) {
+                let node_main = document.getElementById(`partners_and_sponsers_images_${str(p.Rank).toLowerCase() }`)
+
+                let div = document.createElement("div")
+                div.className = `${p.Rank} animatedGlow grid-item`
+                fetchAndSetBackgroundImage(p.IMG, div)
+                div.addEventListener('mouseover', (event) => {
+                    console.log("Fireworks of type " + p.Rank + " for " + p.Name)
+                });
+                div.setAttribute('url', `${p.url}`)
+                div.addEventListener('click', (event) => {
+                    window.location.href = div.getAttribute('url')
+                });
+                node_main.append(div)
+            }
+
+        })
+}
+
+function changeFocusPartner(target) {
+    document.getElementById('partner-and-sponsors-GOLD').style.display = 'none'
+    document.getElementById('partner-and-sponsors-SILVER').style.display = 'none'
+    document.getElementById('partner-and-sponsors-BRONZE').style.display = 'none'
+
+    document.querySelectorAll(".podium-switch div").forEach((element, index) => {
+        element.className = ""
+    })
+
+    target.style.display = 'grid'
+    event.target.className = "box"
+
+
+}
+
+function getScheduleContent() {
+    scheduleEvents1 = []
+    scheduleEvents2 = []
+    fetch('./schedule.json')
+        .then(response => response.json())
+        .then(data => {
+            scheduleData = data;
+            let node_main = document.getElementById("schedule-day-content")
+            for (e of scheduleData.Day1) {
+                let event = new Event(node_main)
+                event.setTime(e.Time)
+                event.setTalk(e.Talk)
+                event.setLocation(e.Location)
+                event.setKnowMore(e.AboutIt)
+                event.setAvatar(e.Avatar)
+
+                event.bind()
+                event.display()
+                scheduleEvents1.push(event)
+            }
+            for (e of scheduleData.Day2) {
+                let event = new Event(node_main)
+                event.setTime(e.Time)
+                event.setTalk(e.Talk)
+                event.setLocation(e.Location)
+                event.setKnowMore(e.AboutIt)
+                event.setAvatar(e.Avatar)
+
+                event.bind()
+                scheduleEvents2.push(event)
+
+            }
+        });
+}
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
+function showScheduleDay(n) {
+    if (scheduleData == null) {
+        getScheduleContent()
+    } else {
+
+        if (n === 1) {
+            document.getElementById("day1").classList.add("selected")
+            document.getElementById("day2").classList.remove("selected")
+
+            console.log("displaying schedule 1")
+            for (e of scheduleEvents1) {
+                e.display()
+            }
+            for (e of scheduleEvents2) {
+                e.hide()
+            }
+
+
+        } else if (n === 2) {
+            document.getElementById("day2").classList.add("selected")
+            document.getElementById("day1").classList.remove("selected")
+
+            console.log("displaying schedule 2")
+            for (e of scheduleEvents1) {
+                e.hide()
+            }
+            for (e of scheduleEvents2) {
+                e.display()
+
+
+            }
+        }
+
+    }
+}
+
+class Event {
+    constructor(node_main) {
+        this.content = document.createElement("div")
+        this.avatar = document.createElement("div")
+        this.talk = document.createElement("div")
+        this.time_location = document.createElement("div")
+        this.time = document.createElement("div")
+        this.location = document.createElement("div")
+        this.know_more = document.createElement("div")
+
+        this.content.className = "content"
+        this.avatar.className = "avatar"
+        this.talk.className = "talk"
+        this.time_location.className = "time-location"
+        this.time.className = "time"
+        this.location.className = "location"
+        this.know_more.className = "know-more"
+        this.node_main = node_main
+
+        this.know_more.innerText = "ABOUT IT"
+        this.know_more.setAttribute("data-toggle", "modal")
+        this.know_more.setAttribute("data-target", "#exampleModalCenter")
+
+        this.know_more.addEventListener("click", () => {
+
+
+            document.getElementById("modal-title").innerText = `What this talk is about 🎯`
+            document.getElementById("modal-content").innerText = `${this.content_know_more}`
+
+
+        })
+    }
+    setAvatar(content) {
+        fetchAndSetBackgroundImage(content, this.avatar)
+
+    }
+    setTalk(content) {
+        this.talk.innerHTML = content
+    }
+    setTime(content) {
+        this.time.innerHTML = content
+
+    }
+    setLocation(content) {
+        this.location.innerHTML = content
+
+    }
+    setKnowMore(content) {
+
+        this.content_know_more = content
+    }
+    bind() {
+        this.time_location.appendChild(this.time).appendChild(this.location)
+        this.content.appendChild(this.avatar)
+        this.content.appendChild(this.talk)
+        this.content.appendChild(this.time_location)
+        this.content.appendChild(this.know_more)
+        this.node_main.appendChild(this.content)
+    }
+
+    display() {
+        this.content.style.display = "flex"
+    }
+    hide() {
+        this.content.style.display = "none";
+    }
+
+
+}
+
+function fetchAndSetBackgroundImage(imageUrl, target) {
+
+
+
+    target.style.backgroundImage = `url(${imageUrl})`;
+    target.style.backgroundSize = "contain";
+    target.style.backgroundPositionX = "center"
+    target.style.backgroundPositionY = "center"
+    target.style.backgroundRepeat= "no-repeat"
+}
